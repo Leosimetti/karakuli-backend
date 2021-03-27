e672e4e572ab32b31d327b13323e51bede2094e1
nikololiahim <olomishcak@outlook.com>
2021-03-27 22:37:03 +0300

    KAR-32 word parser now works as a generator

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
 from urllib import request
-import pprint
 
 WIKIPEDIA = r"https://en.wikipedia.org/wiki/List_of_j%C5%8Dy%C5%8D_kanji"
 
-if __name__ == "__main__":
-    words = {}
+
+def words():
     with request.urlopen(WIKIPEDIA) as req:
         soup = BeautifulSoup(req.read().decode("utf-8"), features="html.parser")
         table = soup.find_all("table")[1]
@@ -15,7 +14,7 @@ if __name__ == "__main__":
             data = list(map(lambda tag: tag.text, row.find_all("td")))
             if len(data) == 9:
                 word_entry = {
-                    "id": data[0],
+                    "id": int(data[0]),
                     "new": data[1],
                     # "old": data[2],
                     "radical": data[3],
@@ -26,6 +25,4 @@ if __name__ == "__main__":
                     "readings": data[8].strip(),
                 }
 
-                words.setdefault(int(data[0]), word_entry)
-
-        pprint.pprint(words)
+                yield word_entry
