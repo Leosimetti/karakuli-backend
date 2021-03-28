b4849362f4b168b8019307dfede3498c5018d2e8
nikololiahim <olomishcak@outlook.com>
2021-03-28 17:47:19 +0300

    KAR-36 basic deployment using docker-compose

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 import os
 import motor.motor_asyncio
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb://localhost:27017")
+DATABASE_URL = os.getenv("DB_HOST", "mongodb://localhost:27017")
 
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
