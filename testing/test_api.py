b5f5164c03ff15423d037b2823433de3c89badbb
nikololiahim <olomishcak@outlook.com>
2021-03-30 00:35:12 +0300

    KAR-41 figuring out how to test async functions

--- testing/test_api.py
+++ testing/test_api.py
@@ -1,5 +1,6 @@
-import unittest
+import asynctest
 import logging
+import inspect
 import sys
 from fastapi.testclient import TestClient
 from app.app import app
@@ -18,47 +19,47 @@ logger.addHandler(ch)
 client = TestClient(app)
 
 
-class APITestCase(unittest.TestCase):
+def logs(func):
+    if inspect.iscoroutinefunction(func):
+        async def wrapper(*args, **kwargs):
+            logger.debug(f"async {func.__name__}")
+            return await func(*args, **kwargs)
+    else:
+        def wrapper(*args, **kwargs):
+            logger.debug(f"{func.__name__}")
+            return func(*args, **kwargs)
 
-    def tearDown(self) -> None:
-        logger.debug(self.tearDown.__name__)
+    return wrapper
+
+
+class APITestCase(asynctest.TestCase):
+
+    @logs
+    async def tearDown(self) -> None:
         pass
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        logger.debug(cls.tearDownClass.__name__ + "\n")
-        # os._exit(0)
+    @logs
+    async def test_async(self) -> None:
+        import asyncio
+        await asyncio.sleep(1)
+        assert True
+
+    @logs
+    def test_sync(self):
+        assert True
+
+    @logs
+    async def setUp(self) -> None:
         pass
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        logger.debug(cls.setUpClass.__name__)
-        pass
-
-    def setUp(self) -> None:
-        logger.debug(self.setUp.__name__)
-        pass
-
-    def test_review_database_deletion(self):
-        logger.debug(self.test_review_database_deletion.__name__)
-        assert True
-
-    def test_stuff_1(self):
-        logger.debug(self.test_stuff_1.__name__)
-        assert True
-
-    def test_stuff_2(self):
-        logger.debug(self.test_stuff_2.__name__)
-        assert True
-
-
-class AnotherTestCase(APITestCase):
-
-    def test_another_testcase(self):
-        logger.debug(self.test_another_testcase.__name__)
-        assert True
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        logger.debug(cls.tearDownClass.__name__ + "\n")
-        os._exit(0)
+    # def test_review_database_deletion(self):
+    #     logger.debug(self.test_review_database_deletion.__name__)
+    #     assert True
+    #
+    # def test_stuff_1(self):
+    #     logger.debug(self.test_stuff_1.__name__)
+    #     assert True
+    #
+    # def test_stuff_2(self):
+    #     logger.debug(self.test_stuff_2.__name__)
+    #     assert True
