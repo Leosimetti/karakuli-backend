2b4e95db1c5bb11310e111102e800c9101720d31
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2021-07-29 20:32:48 +0700

    removed ping
    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,6 @@ import os
 import libzt
 import time
 
-import platform    # For getting the operating system name
-import subprocess  # For executing a shell command
-
-def ping(host):
-    """
-    Returns True if host (str) responds to a ping request.
-    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
-    """
-
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
-
-    return subprocess.call(command) == 0
 
 if __name__ == "__main__":
     node = libzt.ZeroTierNode()
@@ -41,10 +25,5 @@ if __name__ == "__main__":
     # print("OH YEAHHHHHH")
     ip = node.addr_get_ipv4(net_id)
     print(ip)
-
-    sas = "10.147.18.159"
-    print(ping(sas))
-    print(ping(sas))
-    print(ping(sas))
 
     uvicorn.run("app:app", host=os.getenv("HOST_IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), reload=True)
